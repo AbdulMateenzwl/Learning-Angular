b32ab42c0d9df2967aa17080046c7479ca58a17d
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-06-25 11:45:17 +0000

    Project structure, components, data binding

---
+++ 04_Angular/Basics/src/app/servers/servers.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.scss']
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } , 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
+}
