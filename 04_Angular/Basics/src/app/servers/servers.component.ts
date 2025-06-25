393ab8058fb561458ab1548ffb3e05b378b4df5f
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-06-25 13:03:06 +0000

    Two way binding

--- 04_Angular/Basics/src/app/servers/servers.component.ts
+++ 04_Angular/Basics/src/app/servers/servers.component.ts
@@ -3,19 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  styleUrls: ['./servers.component.scss'],
 })
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
+  serverName = 'test';
 
-  constructor(){
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    } , 2000)
+    }, 2000);
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreationStatus =
+      'Server was created!, Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (event.target as HTMLInputElement).value;
   }
 }
