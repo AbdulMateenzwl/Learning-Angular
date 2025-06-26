8599f8e4b013316903ddfa7e7e9d6587f1d2d703
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-06-26 13:43:51 +0000

    Event Emitter Binding to Parent Component

--- 04_Angular/Project-Recipe-App/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 04_Angular/Project-Recipe-App/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent {
-  recipes:Recipe[] = [
-    new Recipe('Name', 'Description of recipe','https://www.shutterstock.com/image-photo/fried-salmon-steak-cooked-green-600nw-2489026949.jpg')
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipes: Recipe[] = [
+    new Recipe(
+      'Name',
+      'Description of recipe',
+      'https://www.shutterstock.com/image-photo/fried-salmon-steak-cooked-green-600nw-2489026949.jpg'
+    ),
+    new Recipe(
+      'Recipe',
+      'Description of recipe',
+      'https://www.shutterstock.com/image-photo/fried-salmon-steak-cooked-green-600nw-2489026949.jpg'
+    ),
   ];
 
-  constructor(){
+  constructor() {}
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
-
-
 }
