4c87f530fe8cbc961c289575a4ebc90e88484d8d
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-06-27 08:14:26 +0000

    using emiter in service to trigger function on subscriber

--- 04_Angular/Services/src/app/new-account/new-account.component.ts
+++ 04_Angular/Services/src/app/new-account/new-account.component.ts
@@ -12,10 +12,13 @@ export class NewAccountComponent {
   constructor(
     private loggingService: LoggingService,
     private accountService: AccountsService
-  ) {}
+  ) {
+    this.accountService.statusUpdate.subscribe((status: string) =>
+      alert('new status ' + status)
+    );
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountService.addAccount(accountName, accountStatus);
-    // this.loggingService.logStatusChange(accountStatus);
   }
 }
