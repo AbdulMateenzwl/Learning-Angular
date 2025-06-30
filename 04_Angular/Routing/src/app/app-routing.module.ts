0de316d8040108d7818628a027afff69c07befcb
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-06-30 11:39:30 +0000

    static dynamic data, Resolvers

--- 04_Angular/Routing/src/app/app-routing.module.ts
+++ 04_Angular/Routing/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
 import { AuthGuard } from './auth-guard.service';
 import { CanDeactivateGuard } from './servers/edit-server/can-deactivated-guard.service';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -24,7 +26,11 @@ const appRoutes: Routes = [
     component: ServersComponent,
     // for nested routing
     children: [
-      { path: ':id', component: ServerComponent },
+      {
+        path: ':id',
+        component: ServerComponent,
+        resolve: { server: ServerResolver },
+      },
       {
         path: ':id/edit',
         component: EditServerComponent,
@@ -32,9 +38,16 @@ const appRoutes: Routes = [
       },
     ],
   },
+  // {
+  //   path: 'not-found',
+  //   component: PageNotFoundComponent,
+  // },
   {
     path: 'not-found',
-    component: PageNotFoundComponent,
+    component: ErrorPageComponent,
+    data: {
+      message: 'Page not found',
+    },
   },
   {
     path: '**',
