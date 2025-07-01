b1008543ba1c6b01c79acc8fad8490ec84f63a28
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-01 10:47:48 +0000

    Extracting data from form, Radio Buttons

--- 04_Angular/Forms/Forms-Template-Driven/src/app/app.component.ts
+++ 04_Angular/Forms/Forms-Template-Driven/src/app/app.component.ts
@@ -11,11 +11,49 @@ export class AppComponent {
 
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female'];
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // to set the data of the form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: '',
+    //   },
+    //   secret: 'teacher',
+    //   questionAnswer: '',
+    //   gender: 'male',
+    // });
+
+    // to update some values of the form
+    this.signupForm.form.patchValue({
+      userData: {
+        username: 'newUser',
+      },
+    });
   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    if (this.signupForm && this.signupForm.form) {
+      this.submitted = true;
+      this.user.username = this.signupForm.value.userData?.username || '';
+      this.user.email = this.signupForm.value.userData?.email || '';
+      this.user.secretQuestion = this.signupForm.value.secret;
+      this.user.answer = this.signupForm.value.questionAnswer;
+      this.user.gender = this.signupForm.value.gender;
+
+      this.signupForm.reset();
+    }
   }
 }
