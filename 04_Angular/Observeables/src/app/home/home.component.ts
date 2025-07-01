27e237586ce490dd3bbcf3a343df703d15b142f3
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-01 07:08:03 +0000

    Pipes function, map, filter

--- 04_Angular/Observeables/src/app/home/home.component.ts
+++ 04_Angular/Observeables/src/app/home/home.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Observer, Subscription } from 'rxjs';
+import {
+  filter,
+  interval,
+  map,
+  Observable,
+  Observer,
+  Subscription,
+} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +19,15 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    const customeIntervalObserveable = Observable.create(
+    const customeIntervalObserveable: Observable<number> = Observable.create(
       (observer: Observer<number>) => {
         let count = 0;
         setInterval(() => {
           observer.next(count);
-          if (count === 2) {
+          if (count === 7) {
             observer.complete();
           }
-          if (count > 3) {
+          if (count > 5) {
             observer.error(new Error('Count is greater than 3'));
           }
           count++;
@@ -28,18 +35,29 @@ export class HomeComponent implements OnInit, OnDestroy {
       }
     );
 
-    this.firstObsSubscription = customeIntervalObserveable.subscribe(
-      (data: number) => {
-        console.log(data);
-      },
-      (error: Error) => {
-        console.log(error);
-        alert(error.message);
-      },
-      () => {
-        console.log('Completed');
-      }
-    );
+    this.firstObsSubscription = customeIntervalObserveable
+      .pipe(
+        filter((data) => {
+          return +data > 0;
+        })
+      )
+      .pipe(
+        map((data) => {
+          return 'Round ' + (data + 1);
+        })
+      )
+      .subscribe(
+        (data: string) => {
+          console.log(data);
+        },
+        (error: Error) => {
+          console.log(error);
+          alert(error.message);
+        },
+        () => {
+          console.log('Completed');
+        }
+      );
   }
 
   ngOnDestroy(): void {
