8979a25e22ffe3895a68515e66611bd0801c40d4
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-01 06:47:02 +0000

    Making custom observeable and error and complete

--- 04_Angular/Observeables/src/app/home/home.component.ts
+++ 04_Angular/Observeables/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Observable, Subscription } from 'rxjs';
+import { interval, Observable, Observer, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +12,34 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
-    });
+    const customeIntervalObserveable = Observable.create(
+      (observer: Observer<number>) => {
+        let count = 0;
+        setInterval(() => {
+          observer.next(count);
+          if (count === 2) {
+            observer.complete();
+          }
+          if (count > 3) {
+            observer.error(new Error('Count is greater than 3'));
+          }
+          count++;
+        }, 1000);
+      }
+    );
+
+    this.firstObsSubscription = customeIntervalObserveable.subscribe(
+      (data: number) => {
+        console.log(data);
+      },
+      (error: Error) => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log('Completed');
+      }
+    );
   }
 
   ngOnDestroy(): void {
