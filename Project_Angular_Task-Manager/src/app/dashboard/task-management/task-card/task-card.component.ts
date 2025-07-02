c03d4ababfdfcc3e09722db9721e0fd1d309019a
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-02 10:32:13 +0000

    Add delete functionality to task card component

--- Project_Angular_Task-Manager/src/app/dashboard/task-management/task-card/task-card.component.ts
+++ Project_Angular_Task-Manager/src/app/dashboard/task-management/task-card/task-card.component.ts
@@ -1,14 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/models/task.model';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-task-card',
   templateUrl: './task-card.component.html',
-  styleUrls: ['./task-card.component.scss']
+  styleUrls: ['./task-card.component.scss'],
 })
 export class TaskCardComponent {
+  @Input() task!: Task;
 
-  @Input() task!:Task;
+  constructor(private taskService: TaskService) {}
 
-  
+  onDelete() {
+    this.taskService.deleteTask(this.task.id);
+  }
 }
