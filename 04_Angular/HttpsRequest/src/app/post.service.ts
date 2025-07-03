477fd454a9d25eaa95207cc36bf7ab81e1e926a0
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-03 12:56:58 +0000

    observe, header, params, interceptors, tapping request

--- 04_Angular/HttpsRequest/src/app/post.service.ts
+++ 04_Angular/HttpsRequest/src/app/post.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
-import { catchError, map, Subject, throwError } from 'rxjs';
+import { catchError, map, Subject, tap, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
@@ -15,7 +15,10 @@ export class PostsService {
   createAndStorePost(title: string, content: string) {
     const postData: Post = { title: title, content: content };
     this.http
-      .post<{ name: string }>(`${this.dbUrl}/post.json`, postData)
+      .post<{ name: string }>(`${this.dbUrl}/post.json`, postData, {
+        // Get the complete response with status, body and other fields
+        observe: 'response',
+      })
       .subscribe(
         (responseData) => {
           console.log(responseData);
@@ -28,7 +31,14 @@ export class PostsService {
 
   fetchPosts() {
     return this.http
-      .get<{ [key: string]: Post }>(`${this.dbUrl}/post.json`)
+      .get<{ [key: string]: Post }>(`${this.dbUrl}/post.json`, {
+        // passing custom headers
+        headers: new HttpHeaders({
+          'Custom-Header': 'Hello',
+        }),
+        // passing custom parameters
+        params: new HttpParams().set('print', 'pretty'),
+      })
       .pipe(
         map((responseData) => {
           const postArray: Post[] = [];
@@ -43,6 +53,18 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete(`${this.dbUrl}/post.json`);
+    return this.http
+      .delete(`${this.dbUrl}/post.json`, {
+        observe: 'events',
+        responseType:'json'
+      })
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if(event.type === HttpEventType.Response){
+            console.log(event.body)
+          }
+        })
+      );
   }
 }
