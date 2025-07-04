3c107aa35a5a8ecb4856ddbd1698cf1f61b1025a
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2025-07-04 12:00:24 +0000

    Implement authentication flow with login/signup handling and user state management

--- 04_Angular/Project-Recipe-App/src/app/auth/auth.service.ts
+++ 04_Angular/Project-Recipe-App/src/app/auth/auth.service.ts
@@ -1,43 +1,105 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, Subject, tap, throwError } from 'rxjs';
+import { User } from './user.model';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
-  expiresIn: string;
+  expiresIn: number;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  url =
-    'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBcOOk9CUXsGuzBsJhd-FqStXkN4MnJRUw';
+  user = new Subject<User>();
 
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
     return this.http
-      .post<AuthResponseData>(this.url, {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      })
+      .post<AuthResponseData>(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBcOOk9CUXsGuzBsJhd-FqStXkN4MnJRUw',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
       .pipe(
         catchError((errorRes) => {
-          let errorMessge = 'An unknown Error Occurred';
-          if (!errorRes.erro || !errorRes.error.error) {
-            return throwError(errorMessge);
-          }
-          switch (errorRes.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessge = 'This email exists already';
-          }
+          return this.handleError(errorRes);
+        }),
 
-          return throwError(errorMessge);
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            resData.expiresIn
+          );
         })
       );
+  }
+
+  private handleAuthentication(
+    email: string,
+    localId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationData = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, localId, token, expirationData);
+
+    this.user.next(user);
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBcOOk9CUXsGuzBsJhd-FqStXkN4MnJRUw',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          return this.handleError(errorRes);
+        }),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            resData.expiresIn
+          );
+        })
+      );
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    console.log(errorRes);
+    let errorMessage = 'An unknown Error Occurred';
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMessage));
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+
+    return throwError(() => new Error(errorMessage));
   }
 }
